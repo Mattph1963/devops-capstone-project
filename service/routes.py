238a3dc27dbee7fd4c77865e1c757b016e652ab3
Mattph1963 <mphaysom63@gmail.com>
2025-04-13 01:38:30 -0400

    Fix lint issues and add flake8 linting to CI

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -94,6 +95,7 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -126,7 +128,7 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if not account:  # Fix: Return 404 if account doesn't exist
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.delete()
     return "", status.HTTP_204_NO_CONTENT
 

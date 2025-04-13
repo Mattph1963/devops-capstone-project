cee1ceb07608341173bfe65fdce2b0bf0827deac
Mattph1963 <mphaysom63@gmail.com>
2025-04-13 04:32:20 -0400

    Fix linting issues in __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -39,6 +39,7 @@ except Exception as error:  # pylint: disable=broad-except
     # gunicorn requires exit code 4 to stop spawning workers when they die
     sys.exit(4)
 
+
 # Add security headers to all responses
 @app.after_request
 def add_security_headers(response):
@@ -46,5 +47,6 @@ def add_security_headers(response):
     response.headers['X-XSS-Protection'] = '1; mode=block'
     response.headers['X-Content-Type-Options'] = 'nosniff'
     return response
+
 
 app.logger.info("Service initialized!")
